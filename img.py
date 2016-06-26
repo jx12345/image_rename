c4c0b5b00785cadae56459178079f2a2032c7f42
jx12345 <jim@formatc.net>
2016-06-26 12:39:10 +0100

    tidy up

--- img.py
+++ img.py
@@ -1,21 +1,15 @@
 #!/usr/bin/env python3.5
+import os, pprint, PIL.Image, sys
 
-import os
-import pprint
-import PIL.Image
-import sys
-
-pre_dir = ""
-post_dir = ""
+pre_dir = "."
+post_dir = "."
 print(str(sys.argv))
 
 if (len(sys.argv) == 3):
   pre_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), sys.argv[1])
   post_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), sys.argv[2])
 
-print("pre_dir: " + pre_dir)
-print("post_dir: " + post_dir)
-print()
+print("pre_dir: " + pre_dir + ", post_dir: " + post_dir + "\n")
 
 for file in os.listdir(pre_dir):
     path = os.path.join(pre_dir, file)
@@ -23,6 +17,5 @@ for file in os.listdir(pre_dir):
     img = PIL.Image.open(path)
     exif_data = img._getexif()
     name_list = exif_data[36867].split()
-    print(name_list)
     new_name = name_list[0].replace(':', '-') + ' ' + name_list[1].replace(':', '.') + '.jpg'
     print(new_name + "\n")
